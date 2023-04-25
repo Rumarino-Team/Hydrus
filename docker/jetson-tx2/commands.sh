9dfda34894f4b31af78ee926e7245d9cfb8f59ad
aquino35 <osvaldo.aquino@upr.edu>
2023-04-25 06:33:30 -0400

    Some local changes before HH#2

--- docker/jetson-tx2/commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# This script is used to run commands on the docker 
-
-source /opt/ros/noetic/setup.sh
-roscore&
-roslaunch nav_sensors hydrus_dvl.launch&
-rqt_graph
--- jetson-tx2/catkin_ws/src/nav_sensors/scripts/dvl_sub.py
+++ jetson-tx2/catkin_ws/src/nav_sensors/scripts/dvl_sub.py
@@ -1,33 +1,34 @@
 # Copyright (c) 2023 Real-Time Development Center (RTDC) Project
 # All rights reserved.
 #!/usr/bin/env python3
+# Simple ros script to verify publisher
 import rospy
 from nav_sensors.msg import DVL_MSG
 
 def dvl_cb(dvl_msg):
-    rospy.loginfo(rospy.get_caller_id() + "Velocities %s", dvl_msg.data.velocity)
-    rospy.loginfo(rospy.get_caller_id() + "Velocity Error %s", dvl_msg.data.vel_error)
-    rospy.loginfo(rospy.get_caller_id() + "Beams %s", dvl_msg.data.beams)
-    rospy.loginfo(rospy.get_caller_id() + "Mean Bottom Range %s", dvl_msg.data.mean_bottom_range)
-    rospy.loginfo(rospy.get_caller_id() + "Speed of Sound %s", dvl_msg.data.speed_of_sound)
-    rospy.loginfo(rospy.get_caller_id() + "Input Voltage %s", dvl_msg.power.input_voltage)
-    rospy.loginfo(rospy.get_caller_id() + "Transmit Voltage %s", dvl_msg.power.transmit_voltage)
-    rospy.loginfo(rospy.get_caller_id() + "Transmit Current %s", dvl_msg.power.transmit_current)
+
+    #log measurements of type dvl_msg:
+    rospy.loginfo("Velocities %s", dvl_msg.data.velocity)
+    rospy.loginfo("Velocity Error %s", dvl_msg.data.vel_error)
+    rospy.loginfo("Beams %s", dvl_msg.data.beams)
+    rospy.loginfo("Mean Bottom Range %s", dvl_msg.data.mean_bottom_range)
+    rospy.loginfo("Speed of Sound %s", dvl_msg.data.speed_of_sound)
+    rospy.loginfo("Input Voltage %s", dvl_msg.power.input_voltage)
+    rospy.loginfo("Transmit Voltage %s", dvl_msg.power.transmit_voltage)
+    rospy.loginfo("Transmit Current %s", dvl_msg.power.transmit_current)
 
     
 def listener():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
+    # initialize node
     rospy.init_node('listener', anonymous=True)
 
+    # create subscriber
     rospy.Subscriber("/hydrus/dvl", DVL_MSG, dvl_cb)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
+    # start subscriber
     listener()
